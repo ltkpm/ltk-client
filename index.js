c8d9a5ce0bba286587766c76f61820b3ade5eaea
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-01-05 03:16:38 +0100

    Add question for register new repository

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var program = require("commander")
 var Manager = require("./cli/Manager").Manager
+var inquirer = require("inquirer")
 
 var repoManager = new Manager()
 
@@ -11,31 +12,56 @@ program
   .command("install <repo>")
   .action(function(repo) {
     let scope = undefined
-    if(program.save){
+    if (program.save) {
       scope = 2
-    }
-    else {
+    } else {
       scope = 1
     }
-    repoManager.install(repo,scope)
-  });
+    repoManager.install(repo, scope)
+  })
 
 program
   .option("-g, --global", "global installation")
   .command("remove <repo>")
   .action(function(repo) {
-    console.log(repo);
-  });
+    console.log(repo)
+  })
 
-program
-  .option("-g, --global", "global installation")
-  .command("register <repo>")
-  .action(function(repo) {
-    console.log(repo);
-  });
+program.command("register").action(function(repo) {
+  var questions = [
+    {
+      type: "input",
+      name: "name",
+      message: "The name of repository",
+    },
+    { type: "input", name: "url", message: "Url of repository" },
+    {
+      type: "list",
+      name: "type",
+      message: "Type of repository",
+      choices: ["Node", "Python"],
+      filter: function(val) {
+        return val.toLowerCase()
+      }
+    },
+    {
+      type: "input",
+      name: "version",
+      message: "Version of repository",
+      validate: function(value) {
+        var valid = !isNaN(parseFloat(value))
+        return valid || "Please enter a number"
+      },
+      filter: Number
+    }
+  ]
 
+  inquirer.prompt(questions).then(answers => {
+    repoManager.register(JSON.stringify(answers, null, "  "))
+  })
+})
 
-program.parse(process.argv);
+program.parse(process.argv)
 
 /*
   install
