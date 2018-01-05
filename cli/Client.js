c033f004f1177ae8efcb44cd3591ea02e967433a
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-01-05 03:13:15 +0100

    Add default content type, add method to register new repository

--- cli/Client.js
+++ cli/Client.js
@@ -6,16 +6,37 @@ var Repository = require("../Model/Repository").Repository;
 class Client {
   constructor() {
     this.URL = server.url + "/api"
+    axios.defaults.headers.post['Content-Type'] = 'application/json'
   }
 
   getRepository(repo_name) {
-    var url = this.URL + "/repos/" + repo_name
-    var p = new Promise(function(resolve, reject) {
-        axios.get(url).then(function(response) {
-            resolve(new Repository(response.data));
-        });
+    let url = this.URL + "/repos/" + repo_name
+    let p = new Promise(function(resolve, reject) {
+      axios.get(url).then(function(response) {
+        resolve(new Repository(response.data))
+      })
     })
-    return p;
+    return p
+  }
+
+  addRepository(repo) {
+    let url = this.URL + "/repos/"
+    axios.post(url, repo).then(function(response) {
+      let result = undefined
+      switch (response.statusCode) {
+        case 400:
+          result = "Oh no! Something went wrong " + "Reason -> " + response.data
+          break;
+        
+        default:
+          result = "Yay, registered correctly! "
+          break;     
+      }
+      console.log(result)
+    }).catch((response)=>{
+      console.log("Something wrong happen! Reason " )
+      console.log(response)
+    })
   }
 }
 
